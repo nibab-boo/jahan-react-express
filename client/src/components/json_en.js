a7e5ca5e0bedfdcf79af2ddd5ede9d6b295a5a1a
Babin Bohora <bohorababin1@gmail.com>
2022-04-16 00:31:09 +0900

    two json file added

---
+++ client/src/components/json_en.js
@@ -0,0 +1,63 @@
+export const jsonEn = {
+  name: "Jahan Indo Nepal Restaurant",
+  contact: {
+    phone: "0489155644",
+    address: "〒342-0041 Saitama, Yoshikawa, Ho, 1 Chome−２７−4",
+    directionBtn: "Get direction",
+    callBtn: "Make a Call"
+  },
+  footer: {
+    name: "Reach out to",
+    subList: [
+      {
+        name: "Connect",
+        items: [
+          {
+            item: "Instagram",
+            link: "www.instagram.com"
+          },
+          {
+            item: "Twitter",
+            link: "www.instagram.com"
+          },
+          {
+            item: "Facebook",
+            link: "www.instagram.com"
+          }
+        ]
+      },{
+        name: "Order",
+        items: [
+          {
+            item: "Phone",
+            link: "www.instagram.com"
+          },
+          {
+            item: "Uber",
+            link: "www.uber.com"
+          },
+          {
+            item: "Demaekan",
+            link: "www.demaekan.com"
+          }
+        ]
+      },{
+        name: "Order",
+        items: [
+          {
+            item: "Phone",
+            link: "www.instagram.com"
+          },
+          {
+            item: "Hotpepper",
+            link: "www.Hotpepper.com"
+          },
+          {
+            item: "Yahoo",
+            link: "www.Yahoo.com"
+          }
+        ]
+      }
+    ]
+  },
+}
---
+++ client/src/components/json_jp.js
@@ -0,0 +1,63 @@
+export const jsonJp = {
+  name: "ジャハンインドネパール料理",
+  contact: {
+    phone: "0489155644",
+    address: "〒342-0041 埼玉県吉川市保、1丁目-27-4",
+    directionBtn: "方向性を得る",
+    callBtn: "電話を掛ける"
+  },
+  footer: {
+    name: "に手を差し伸べる",
+    subList: [
+      {
+        name: "接続",
+        items: [
+          {
+            item: "Instagram",
+            link: "www.instagram.com"
+          },
+          {
+            item: "Twitter",
+            link: "www.instagram.com"
+          },
+          {
+            item: "Facebook",
+            link: "www.instagram.com"
+          }
+        ]
+      },{
+        name: "注文",
+        items: [
+          {
+            item: "Phone",
+            link: "www.instagram.com"
+          },
+          {
+            item: "Uber",
+            link: "www.uber.com"
+          },
+          {
+            item: "Demaekan",
+            link: "www.demaekan.com"
+          }
+        ]
+      },{
+        name: "予約",
+        items: [
+          {
+            item: "Phone",
+            link: "www.instagram.com"
+          },
+          {
+            item: "Hotpepper",
+            link: "www.Hotpepper.com"
+          },
+          {
+            item: "Yahoo",
+            link: "www.Yahoo.com"
+          }
+        ]
+      }
+    ]
+  }
+}
